5f8cd66ea0562c67e5bc0106f0a27665ddfc4ee0
KIATTISAK PIMARPORN <enterpannet@gmail.com>
2023-12-28 14:51:51 +0700

    hh

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,23 +1,49 @@
+import { useSelector, useDispatch } from "react-redux";
+import { addText, deleteText } from "../slices/messageBoardSlice";
 function MessageBoard() {
+  const text = useSelector((state) => {
+    return state.textSlice;
+  });
+  const dispatch = useDispatch();
+  const handleDelete = (index) => {
+    dispatch(deleteText(index));
+  };
   return (
     <div className="app-wrapper">
       <h1 class="app-title">Message board</h1>
       <div class="message-input-container">
-        <label>
-          <input
-            id="message-text"
-            name="message-text"
-            type="text"
-            placeholder="Enter message here"
-          />
-        </label>
-        <button className="submit-message-button">Submit</button>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            e.target[0].value ? dispatch(addText(e.target[0].value)) : null;
+            e.target[0].value = "";
+          }}
+        >
+          <label>
+            <input
+              id="message-text"
+              name="message-text"
+              type="text"
+              placeholder="Enter message here"
+            />
+          </label>
+          <button className="submit-message-button">Submit</button>
+        </form>
       </div>
       <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+        {text.map((item, index) => {
+          return (
+            <div key={index} className="message">
+              <h1>{item}</h1>
+              <button
+                className="delete-button"
+                onClick={() => handleDelete(index)}
+              >
+                x
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/slices/messageBoardSlice.jsx
+++ src/slices/messageBoardSlice.jsx
@@ -1,1 +1,21 @@
-// TODO: Start Create Slice Here
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+
+export const textSlice = createSlice({
+  name: "textSlice",
+  initialState: initialState,
+  reducers: {
+    addText: (state, action) => {
+      state = [...state, action.payload];
+      return state;
+    },
+    deleteText: (state, action) => {
+      state.splice(action.payload, 1);
+    },
+  },
+});
+
+export const { addText, deleteText } = textSlice.actions;
+
+export default textSlice.reducer;
